0f858afb11b8563cebd6f7394496cff588d8ca46
Joshua Williams <jmw0426@hotmail.com>
2020-06-09 20:47:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   extend Findable
-  
+  include Memorable::InstanceMethods
   include Paramable
   attr_accessor :name
   attr_reader :artist
